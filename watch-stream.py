8d6aa2c7fc5ae76702aea0126859c2d4a866b085
Maël Valais <mael@vls.dev>
2024-04-29 16:15:46 +0200

    watch-stream.py: document Vault crashing with connecting to kube-apiserver

--- watch-stream.py
+++ watch-stream.py
@@ -1,13 +1,18 @@
 """
-This is a tiny mitmproxy script that enables the streaming mode [1]
-whenever a request contains the query parameter ?watch=true. The
-watch=true parameter is passed by Kubernertes clients (such as client-go)
-when they intend to be notified of object updates.
+This is a tiny mitmproxy script that enables the streaming mode [1] whenever a
+request contains the query parameter ?watch=true. The watch=true parameter is
+passed by Kubernertes clients (such as client-go) when they intend to be
+notified of object updates.
 
 Use with:
    mitmproxy -p 9090 -s watch-stream.py
 
 [1]: https://docs.mitmproxy.org/stable/overview-features/#streaming
+
+Note: mitmproxy crashes with "ConnectionInputs.RECV_RST_STREAM in state
+ConnectionState.CLOSED" when vault tries to establish a connection with the
+kube-apiserver. This issue seems to be a problem with "flow.request.stream =
+True": https://github.com/mitmproxy/mitmproxy/issues/5897.
 """
 
 from mitmproxy import ctx, http
